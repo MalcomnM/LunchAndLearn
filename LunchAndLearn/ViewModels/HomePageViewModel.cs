b29f027e5b2bd59d0df5bab8f86d220f5b2d5ec1
Malcomn <malcomn.j.mitchell@gmail.com>
2022-09-08 02:30:26 -0400

    update

--- LunchAndLearn/ViewModels/HomePageViewModel.cs
+++ LunchAndLearn/ViewModels/HomePageViewModel.cs
@@ -9,48 +9,17 @@ namespace LunchAndLearn.ViewModels;
 public partial class HomePageViewModel: ObservableObject
 {
     [ObservableProperty]
-    private string firstName;
-
-    [ObservableProperty]
-    private string lastName;
-
-    [ObservableProperty]
-    private int count;
-
-    [ObservableProperty]
-    private ObservableCollection<Character> characters = new ObservableCollection<Character>();
-
-    private ICharacterApi _characterApi;
+    private string entryTest;
 
 
-    public HomePageViewModel(ICharacterApi characterApi)
+    public HomePageViewModel()
     {
-        _characterApi = characterApi;
+  
     }
 
     [RelayCommand]
-    private void SayHello()
+    private void TestAsync()
     {
-        Console.WriteLine("HELLO WORLD");
-    }
-
-    [RelayCommand]
-    private void Add()
-    {
-        Count++;
-    }
-
-    [RelayCommand]
-    private async Task LoadCharactersAsync()
-    {
-        GetCharactersResult result = await _characterApi.Characters(1);
-        
-        if(result.Results.Count != 0)
-        {
-            foreach (Character character in result.Results)
-            {
-                Characters.Add(character);
-            }
-        }
+        Console.WriteLine("Hello World");
     }
 }
